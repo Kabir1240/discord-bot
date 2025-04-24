a1900c8bca6d098c955d342b700d5abd7faa3946
Kabir Kashif <kashif.kabir60@gmail.com>
2025-04-24 11:26:13 +0500

    completed experimenting. I might make this a fully fledged bot at some point

--- main.py
+++ main.py
@@ -49,11 +49,67 @@ async def hello(ctx):
 @bot.command()
 async def assign(ctx):
     role = discord.utils.get(ctx.guild.roles, name=selected_role)
+    user_roles = ctx.author.roles
+    for user_role in user_roles:
+        if user_role.name == selected_role:
+            await ctx.send(f"{ctx.author.mention} is already assigned to {selected_role}")
+            return
+
     if role:
         await ctx.author.add_roles(role)
-        print(role)
         await ctx.send(f"{ctx.author.mention} is now assigned to {selected_role}")
     else:
         await ctx.send(f"Couldn't find {selected_role}")
+
+
+@bot.command()
+async def unassign(ctx):
+    role = discord.utils.get(ctx.guild.roles, name=selected_role)
+    if role:
+        user_roles = ctx.author.roles
+        for user_role in user_roles:
+            if user_role.name == selected_role:
+                await ctx.author.remove_roles(role)
+                await ctx.send(f"{selected_role} is now removed from {ctx.author.mention}")
+                return
+        await ctx.send(f"{ctx.author.mention} doesn't have the {selected_role}")
+    else:
+        await ctx.send(f"Couldn't find {selected_role}")
+
+
+@bot.command()
+@commands.has_role(selected_role)
+async def secret(ctx):
+    await ctx.send("Welcome to the club!")
+
+
+@secret.error
+async def secret_error(ctx, error):
+    if isinstance(error, commands.MissingRole):
+        await ctx.send("you do not have permission to do that!")
+
+
+@bot.command()
+async def dm(ctx, *, msg):
+    await ctx.author.send(f"You said {msg}")
+
+
+@bot.command()
+async def reply(ctx):
+    await ctx.reply("This is a reply to your message")
+
+
+@dm.error
+async def dm_error(ctx, error):
+    print(error)
+
+
+@bot.command()
+async def poll(ctx, *, question):
+    embed = discord.Embed(title="polling", description=question)
+    poll_message = await ctx.send(embed=embed)
+    await poll_message.add_reaction("👍")
+    await poll_message.add_reaction("👎")
+
 
 bot.run(token, log_handler=handler, log_level=logging.DEBUG)
